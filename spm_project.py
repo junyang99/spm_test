e9d6fcfc1780829195b9401b7272da85fb246118
Joel Tan <114053402+Yakashimaru@users.noreply.github.com>
2023-09-12 14:24:32 +0800

    Create spm_project.py

---
+++ spm_project.py
@@ -0,0 +1,18 @@
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+    prime_numbers = [p for p in range(2, n + 1) if primes[p]]
+    return prime_numbers
+
+def find_large_primes():
+    n = 100000  # You can change this value to generate prime numbers up to a different limit
+    result = sieve_of_eratosthenes(n)
+    return result
+
+large_primes = find_large_primes()
+print(large_primes)
